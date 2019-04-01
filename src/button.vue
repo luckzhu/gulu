53f826bbe38a7baa951e5ac3255648f2e5899c41
luckzhu <luckzhu@1992@gmail.com>
2019-04-01 18:05:06 +0800

    添加button基本组件

---
+++ src/button.vue
@@ -0,0 +1,22 @@
+<template>
+        <button class="g-button">
+            <slot></slot>
+        </button>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style>
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: var(--padding);
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        vertical-align: middle;
+        color: var(--color);
+    }
+</style>
