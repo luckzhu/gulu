d0969d86b317c9776b1ca56e0b184c188959bb5e
luckzhu <luckzhu@1992@gmail.com>
2019-04-02 17:45:59 +0800

    创建buttonGroup

---
+++ src/button-group.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss">
+    .g-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+        & >.g-button {
+            border-radius: 0;
+
+            &:first-child {
+                border-top-left-radius: 4px;
+                border-bottom-left-radius: 4px;
+            }
+            &:last-child {
+                border-top-right-radius: 4px;
+                border-bottom-right-radius: 4px;
+            }
+
+            &:not(:first-child){
+                margin-left: -1px;
+            }
+
+            &:hover {
+                z-index: 1;
+            }
+
+        }
+
+
+
+
+    }
+
+</style>
