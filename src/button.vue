9ffbb0089f5a24bdfb0991b149ecfe60bcf12dcf
luckzhu <luckzhu@1992@gmail.com>
2019-04-02 15:58:22 +0800

    添加icon和iconPosition

--- src/button.vue
+++ src/button.vue
@@ -1,22 +1,51 @@
 <template>
-        <button class="g-button">
-            <slot></slot>
+
+        <button class="g-button" v-bind:class="{[iconPosition]:true}" >
+            <svg v-if="icon" class="icon" >
+                <use v-bind:xlink:href="`#icon-${icon}`"></use>
+            </svg>
+            <div class="content">
+                <slot></slot>
+            </div>
         </button>
+
 </template>
 
 <script>
-    export default {}
+    export default {
+        props: ['icon','iconPosition']
+    }
 </script>
 
-<style>
+<style lang="scss">
     .g-button {
         font-size: var(--font-size);
+        color: var(--color);
+        padding: 0 1em;
+        vertical-align: middle;
+
         height: var(--button-height);
-        padding: var(--padding);
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
-        vertical-align: middle;
-        color: var(--color);
+
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+
+        &.left {
+            >.icon {order: 1; margin-left: 0; margin-right: .4em;}
+            >.content {order: 2;}
+        }
+
+        &.right {
+            >.icon {order: 2; margin-left: .4em; margin-right: 0;}
+            >.content {order: 1;}
+        }
+
+
+        &:hover {border: 1px solid var(--border-hover-color);}
+        &:active {background: var(--border-active-bg);}
+        &:focus {outline: none;}
     }
 </style>
